c3d49e897729552c48f3ee8da095378d5f7915b1
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2024-06-17 11:47:58 +0300

    commit3

--- RemindingMySelf.php
+++ RemindingMySelf.php
@@ -8,9 +8,19 @@ if(!isset($person['name'])){
        $person['name'];
 }
 
-echo "Your name is $person[name]\n";
+echo "Your name is $person[name]".'<br>';
 
-
+   $role='xx';
+   switch($role){
+       case 'admin':
+           echo "Your role is $role\n";
+           break;
+       case 'user':
+           echo "Your role is $role\n";
+           break;
+       default:
+           echo "Not recognised by the system\n";
+   }
 
 
 
