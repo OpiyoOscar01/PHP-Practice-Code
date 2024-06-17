65ef3904857fec7b8e2dc36c1fa4880cb85acfe6
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2024-06-17 11:44:01 +0300

    commit2

---
+++ RemindingMySelf.php
@@ -0,0 +1,21 @@
+<?php
+$person=[
+    'name'=>null
+] ;
+if(!isset($person['name'])){
+     $person['name']="Unknown";
+}  else{
+       $person['name'];
+}
+
+echo "Your name is $person[name]\n";
+
+
+
+
+
+
+
+
+
+?>
